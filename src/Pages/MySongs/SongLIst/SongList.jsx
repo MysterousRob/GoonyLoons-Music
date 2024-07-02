6be7674572376917ca0fc4f7b355fe3a9bcb8560
MysteriousRob <robertjohngasior@gmail.com>
2024-07-02 22:07:30 +0200

    connected all of the links to the media on where to find just need to fix cause they dont work yetConnect media links to songs (pending fixes)
    - Linked songs to their respective media sources
    - Identified issues with link functionality; need to resolve them

--- src/Pages/MySongs/SongLIst/SongList.jsx
+++ src/Pages/MySongs/SongLIst/SongList.jsx
@@ -6,199 +6,202 @@ const songs = [
       artistName: "GoonyLoons |2024 Dark Trap",
       songName: "Float",
       youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goonyloons-float?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "", 
     },
     {
       artistImage: "",
       artistName: "GoonyLoons |2024 Dark Trap",
       songName: "Buss Down",
       youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goonyloons-buss-down-1?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "",
     },
     {
       artistImage: "",
       artistName: "GoonyLoons |2024 Dark Trap",
       songName: "That's Me",
       youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goonyloons-thats-me?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "",
     },
     {
       artistImage: "",
       artistName: "GoonyLoons |2024 Dark Trap",
       songName: "Double Time",
       youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goonyloons-double-time?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "",
     },
     {
       artistImage: "",
       artistName: "GoonyLoons |2024 Dark Trap",
       songName: "Open Up Your Eyes",
       youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goonyloons-open-up-your-eyes?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "",
     },
     {
       artistImage: "",
       artistName: "GoonyLoons |2024 Dark Trap",
       songName: "Revenge For What I Want",
       youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
-    },
-    {
-      artistImage: "",
-      artistName: "GoonyLoons |2024 Dark Trap / Type Beat",
-      songName: "Figment",
-      youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goonyloons-revenge-for-what-i-want?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "",
     },
     {
       artistImage: "",
       artistName: "GoonyLoons |2024 Dark Trap / Type Beat",
       songName: "Foresee",
       youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goonyloons-foresee?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "",
     },
     {
       artistImage: "",
-      artistName: "GoonyLoons |2023 Dark Trap",
+      artistName: "GoonyLoons |2024 Dark Trap / Type Beat",
       songName: "Figment",
       youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goionyloons-figment?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "",
     },
     {
       artistImage: "",
       artistName: "GoonyLoons |2022 Dark Trap / Type Beat",
       songName: "Asteral ",
       youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goonyloons-asteral?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "",
     },
     {
       artistImage: "",
       artistName: "GoonyLoons |2022 Dark Trap",
       songName: "Heroes",
       youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goonyloons-heroes?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "",
     },
     {
       artistImage: "",
       artistName: " GoonyLoons |2022 Dark Trap",
       songName: "Dark Times",
       youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goonyloons-enemy1?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "",
     },
     {
       artistImage: "",
       artistName: "GoonyLoons |2022 Dark Trap",
       songName: "On The Come Up",
       youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goonyloons-on-the-come-up-new-809?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "",
     },
     {
       artistImage: "",
       artistName: "GoonyLoons |2021 Dark Trap",
       songName: "WRLD",
       youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goonyloons-kings-1?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "",
     },
     {
       artistImage: "",
       artistName: "GoonyLoons |2020 Dark Trap / Type Beat",
       songName: "Oops On Fire Prt. 2",
       youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goonyloons-oops-on-fire-prt-2-lil-loaded-type-beat?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "",
+    },
+    {
+      artistImage: "",
+      artistName: "GoonyLoons |2020 Dark Trap / Type Beat",
+      songName: "MTN DEW",
+      youtubeLinks: "https://www.youtube.com/watch?v=V-1i24bsbC4",
+      soundcloudLinks: "https://soundcloud.com/goonyloons/fire-free-goonyloons-mtn-dew?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "",
     },
     {
       artistImage: "",
       artistName: "GoonyLoons |2020 Dark Trap",
       songName: "Chasin",
       youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goonyloons-goonyloons-chasin-with-808-pauses-mastered?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "",
     },
     {
       artistImage: "",
       artistName: "GoonyLoons |2020 Dark Trap",
       songName: "My Gang",
       youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goonyloons-my-gang-3?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "https://open.spotify.com/track/0HuM9NAGqUoUh433Fpy7cH?si=a4bb7a7a958440c3",
     },
     {
       artistImage: "",
       artistName: "GoonyLoons |2020 Dark Trap",
       songName: "Bread",
-      youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      youtubeLinks: " https://www.youtube.com/watch?v=FKFuufwaOG0&list=OLAK5uy_n3naiA5pulPkOJAMjNpu-aEArHRrtxcn0",
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goonyloons-bread2?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "https://open.spotify.com/track/5N4GRyheBdS1XiwaRwoEd5?si=ee0ab4cfa4184df1",
     },
     {
       artistImage: "",
       artistName: "GoonyLoons |2020 Dark Trap",
       songName: "WEDO",
-      youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      youtubeLinks: "https://www.youtube.com/watch?v=60y6id1d5HU&list=OLAK5uy_lUv6bY_gNpYxMcEzs77Ycnt2X6q6mbyxA",
+      soundcloudLinks: "https://soundcloud.com/goonyloons/vabistru-wedo?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "https://open.spotify.com/track/5LzZe7tfWxK0bgDalpF7LO?si=0675eef288cd4f33",
     },
     {
       artistImage: "",
       artistName: "GoonyLoons |2020 Dark Trap",
       songName: "Pull Up",
-      youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      youtubeLinks: "https://www.youtube.com/watch?v=aZUQTH5hYRw&list=OLAK5uy_kf-zVr9NCeNhnuTvwciS-paCO9N9Of9cU",
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goonyloons-pull-up?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "https://open.spotify.com/track/22ovKyKU6cWa2NEENj55uG?si=4da5846db73f432e",
     },
     {
       artistImage: "",
       artistName: "GoonyLoons |2019 Dark Trap",
       songName: "Trouble",
-      youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      youtubeLinks: "https://www.youtube.com/watch?v=x7f-CINJSlc&list=OLAK5uy_nOsOy-UguViYH1QpaDIRY82ViZZ24Vnnw",
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goonyloons-trouble-1?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "https://open.spotify.com/track/1A5pKUYSP2Fsy7UANPbNPr?si=821cd983dcb94b6e",
     },
     {
       artistImage: "",
       artistName: "GoonyLoons |2019 Dark Trap",
       songName: "Slippin",
-      youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      youtubeLinks: "https://www.youtube.com/watch?v=5yWb7sz_SM0&list=OLAK5uy_moC0jCK01bTkDWIl4qQhGmdOdswf1Kj9U",
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goonyloons-slippin?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "https://open.spotify.com/track/7d7vx7ovlfrcdgoLEqYIO3?si=273bbf4719754535",
     },
     {
       artistImage: "",
       artistName: "GoonyLoons |2019 Dark Trap",
       songName: "Purp",
-      youtubeLinks: "",
-      soundcloudLinks: "",
-      spotifyLinks:
+      youtubeLinks: "https://www.youtube.com/watch?v=9VhTVoXUwrQ&list=OLAK5uy_mktc1OdsmlphSoeoBWQYvN9Eeq0vpRyYI",
+      soundcloudLinks: "https://soundcloud.com/goonyloons/goonyloons-purp?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      spotifyLinks: "",
     },
   ];
 
 const SongList = () => {
-  const handleButtonClick = (links) => {
+  const handleButtonClick = (links, platform) => {
     if (links.length > 0) {
       const randomIndex = Math.floor(Math.random() * links.length);
       const randomLink = links[randomIndex];
+      console.log(`Opening ${platform} link: ${randomLink}`);
       window.open(randomLink, '_blank');
-    }
+    } else {
+      console.log(`No ${platform} links available.`);
+    };
   };
   
     return (
       <div className="h-screen flex items-center justify-center">
-        <div className="flex flex-col gap-4 w-full max-h-[597px] max-w-96 overflow-y-auto border border-grey-200 rounded-lg p-4 bg-grey-100">
+        <div className="flex flex-col gap-4 w-full max-h-[700px] max-w-[850px] overflow-y-auto border border-grey-200 rounded-lg p-4 bg-grey-100">
         {songs.map((song, index) => (
           <div key={index} className="flex items-center justify-between p-4 border border-gray-300 rounded-lg bg-white">
             <img src={song.artistImage} alt={song.artistName} className="w-12 h-12 rounded-full object-cover mr-4" />
@@ -207,19 +210,19 @@ const SongList = () => {
               <p className="text-gray-600">{song.artistName}</p>
             </div>
             <div className="flex gap-3">
-              {song.spotifyLinks.length > 0 && (
-                <button className="text-lg" onClick={() => handleButtonClick(song.spotifyLinks)}>
-                  <img src="path/to/spotify-logo.png" alt="Spotify" className="w-6 h-6" />
+            {song.soundcloudLinks.length > 0 && (
+                <button className="text-lg" onClick={() => handleButtonClick(song.soundcloudLinks)}>
+                  <img src="src\assets\Icons\Soundcloudorrrig.png" alt="SoundCloud" className="w-6 h-6" />
                 </button>
               )}
-              {song.soundcloudLinks.length > 0 && (
-                <button className="text-lg" onClick={() => handleButtonClick(song.soundcloudLinks)}>
-                  <img src="path/to/soundcloud-logo.png" alt="SoundCloud" className="w-6 h-6" />
+              {song.spotifyLinks.length > 0 && (
+                <button className="text-lg" onClick={() => handleButtonClick(song.spotifyLinks)}>
+                  <img src="src\assets\Icons\spotifyicon.png" alt="Spotify" className="w-6 h-6" />
                 </button>
               )}
               {song.youtubeLinks.length > 0 && (
                 <button className="text-lg" onClick={() => handleButtonClick(song.youtubeLinks)}>
-                  <img src="path/to/youtube-logo.png" alt="YouTube" className="w-6 h-6" />
+                  <img src="src\assets\Icons\youtuborrig.png" alt="YouTube" className="w-6 h-6" />
                 </button>
               )}
             </div>
